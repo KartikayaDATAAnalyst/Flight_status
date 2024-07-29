78a1e1b3275347778646a8cd13821289f35fceeb
KartikayaDATAAnalyst <kartikayadutt1997@gmail.com>
2024-07-29 19:11:11 +0530

    Update app.jsx

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FlightStatus from './flight_status';
+
+function App() {
+  return (
+    <div>
+      <h1>Flight Status</h1>
+      <FlightStatus />
+    </div>
+  );
+}
+
+export default App;
